8c7b2b567c7ad469d71cd81ff19f0c326d39935a
LordAndrio <bykarandrew@gmail.com>
2023-05-16 22:54:38 +1200

    Established window attributes in main class.

--- Andrew Bykar (5062279) - AS3 (BIT504)/Breakout.java
+++ Andrew Bykar (5062279) - AS3 (BIT504)/Breakout.java
@@ -8,15 +8,26 @@ public class Breakout extends JFrame{
 	
 	private BreakoutPanel panel;
 	
+	String WINDOW_TITLE = "Breakout";
+	Color BACKGROUND_COLOR = Color.WHITE;
+	int WINDOW_HEIGHT = Settings.WINDOW_HEIGHT;
+	int WINDOW_WIDTH = Settings.WINDOW_WIDTH;
+	
 	public Breakout() {
 		// TODO: Set the size of the screen (use Settings.WINDOW_WIDTH/HEIGHT)
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		// TODO: Set the title
+		setTitle(WINDOW_TITLE);
 		// TODO: Set the background colour to white
+		setBackground(BACKGROUND_COLOR);
 		// TODO: Set resizable to false
+		setResizable(false);
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         panel = new BreakoutPanel(this);
         add(panel);
 		// TODO: Set visible to true
+        setVisible(true);
 	}
 
 	public static void main(String[] args) {
